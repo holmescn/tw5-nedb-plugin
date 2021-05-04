1c7e65e25065d75626fd7f4d62f9ca4baadf137a
Holmes Conan <holmesconan@gmail.com>
2021-05-04 17:33:18 +0800

    update: some small fix.

--- filesystemadaptor.js
+++ filesystemadaptor.js
@@ -17,23 +17,11 @@ A sync adaptor module for synchronising with the local filesystem via node.js AP
   // Get a reference to the file system
   if ($tw.node) {
     var path = require("path"),
-      Datastore = require('nedb');
+      DataStore = require('nedb');
   }
 
-  function isSystemTiddler(arg) {
-    var title = arg;
-    if (typeof arg === "object") {
-      title = arg.fields.title;
-    }
-    return /^\$:/.test(title);
-  }
-
-  function isDraftTiddler(arg) {
-    var title = arg;
-    if (typeof arg === "object") {
-      title = arg.fields.title;
-    }
-    return /^Draft of/.test(title);
+  function isSystemTiddler(title) {
+    return /^\$:\//.test(title);
   }
 
   function tiddlerToDocument(tiddler) {
@@ -48,28 +36,28 @@ A sync adaptor module for synchronising with the local filesystem via node.js AP
 
   /**
    * constructor of sync adaptor
-   * 
-   * @param {*} options 
+   *
+   * @param {*} options
    */
   function FileSystemAdaptor(options) {
     var self = this;
     this.wiki = options.wiki;
     this.boot = options.boot || $tw.boot;
     this.logger = new $tw.utils.Logger("filesystem",{colour: "green"});
-    // /StoryList cached in memory
+    // in-memory cache
     this.drafts = {};
     this.memcache = {
-      "$:/StoryList": {}
+      "$:/StoryList": {title: "$:/StoryList"}
     };
     // Create the <wiki>/tiddlers folder if it doesn't exist
     $tw.utils.createDirectory(this.boot.wikiTiddlersPath);
     // Create the <wiki>/tiddlers.db
-    this.db = new Datastore({
+    this.db = new DataStore({
       filename: path.join(this.boot.wikiTiddlersPath, "..", "tiddlers.db"),
       autoload: true,
     });
     // compact the database every 10 minutes.
-    this.db.persistence.setAutocompactionInterval(600 * 1000);
+    //this.db.persistence.setAutocompactionInterval(600 * 1000);
     // wiki title is unique indexed
     this.db.ensureIndex({ fieldName: 'title', unique: true }, function (err) {
       if (err) {
@@ -89,7 +77,7 @@ A sync adaptor module for synchronising with the local filesystem via node.js AP
   /**
    * Gets the supplemental information that the adaptor needs to keep track of for a particular tiddler.
    * For example, the TiddlyWeb adaptor includes a bag field indicating the original bag of the tiddler.
-   * 
+   *
    * @param {*} tiddler Target tiddler
    * @returns an object storing any additional information required by the adaptor.
    */
@@ -109,13 +97,13 @@ A sync adaptor module for synchronising with the local filesystem via node.js AP
    *   filepath: the absolute path to the file containing the tiddler
    *   type: the type of the tiddler file (NOT the type of the tiddler -- see below)
    *   hasMetaFile: true if the file also has a companion .meta file
-   * 
+   *
    * The boot process populates this.boot.files for each of the tiddler files that it loads.
    * The type is found by looking up the extension in $tw.config.fileExtensionInfo (eg "application/x-tiddler" for ".tid" files).
    * It is the responsibility of the filesystem adaptor to update this.boot.files for new files that are created.
-   * 
-   * @param {*} tiddler 
-   * @param {*} callback 
+   *
+   * @param {*} tiddler
+   * @param {*} callback
    */
   FileSystemAdaptor.prototype.getTiddlerFileInfo = function(tiddler,callback) {
     // Always generate a fileInfo object when this fuction is called
@@ -139,7 +127,7 @@ A sync adaptor module for synchronising with the local filesystem via node.js AP
 
   /**
    * Save a tiddler and invoke the callback with (err,adaptorInfo,revision)
-   * 
+   *
    * @param {*} tiddler Tiddler to be saved
    * @param {*} callback function invoked with parameter err,adaptorInfo,revision
    * @param {*} options The tiddlerInfo maintained by the syncer for this tiddler
@@ -147,17 +135,16 @@ A sync adaptor module for synchronising with the local filesystem via node.js AP
   FileSystemAdaptor.prototype.saveTiddler = function(tiddler,callback,options) {
     var self = this;
     var tiddlerInfo = options.tiddlerInfo || {};
-    var title = tiddler.title;
+    var title = tiddler.fields.title;
 
-    this.logger.log(`save "${tiddler.fields.title}"`);
-
-    if (isSystemTiddler(tiddler)) {
+    if (isSystemTiddler(title)) {
       this.getTiddlerFileInfo(tiddler,function(err,fileInfo) {
         if(err) {
           return callback(err);
         }
-        if (tiddler.fields.title === "$:/StoryList") {
-          self.storyList = Object.assign({}, tiddler.fields);
+        if (typeof self.memcache[title] !== "undefined") {
+          self.logger.log(`save "${title}" to in-memory cache`);
+          self.memcache[title] = tiddler.fields;
           return callback(null,fileInfo);
         } else {
           $tw.utils.saveTiddlerToFile(tiddler,fileInfo,function(err,fileInfo) {
@@ -184,14 +171,15 @@ A sync adaptor module for synchronising with the local filesystem via node.js AP
               if(err) {
                 return callback(err);
               }
+              self.logger.log(`save "${title}" to filesystem`);
               return callback(null,fileInfo);
             });
           });
         }
       });
-    
-    } else if(isDraftTiddler(tiddler)) {
+    } else if(typeof tiddler.fields["draft.of"] !== "undefined") {
       this.drafts[title] = tiddler.fields;
+      this.logger.log(`save "${title}" to drafts cache`);
       callback(null, null);
     } else if (this.memcache[title]) {
       this.memcache[title] = tiddler.fields;
@@ -202,6 +190,7 @@ A sync adaptor module for synchronising with the local filesystem via node.js AP
         if (err) {
           self.logger.log(tiddler);
         }
+        self.logger.log(`save "${title}" to NeDB.`);
         callback(err);
       });
     }
@@ -210,41 +199,44 @@ A sync adaptor module for synchronising with the local filesystem via node.js AP
   /**
    * Load a tiddler and invoke the callback with (err,tiddlerFields)
    * We don't need to implement loading for the file system adaptor, because all the tiddler files will have been loaded during the boot process.
-   * 
+   *
    * @param {*} title Title of tiddler to be retrieved
    * @param {*} callback function invoked with parameter err,tiddlerFields
    */
   FileSystemAdaptor.prototype.loadTiddler = function(title,callback) {
-    this.logger.log(`load "${title}"`);
-
+    var self = this;
     if (this.memcache[title]) {
+      this.logger.log(`load "${title}" from in-memory cache`);
       callback(null, this.memcache[title]);
+    } else if (this.drafts[title]) {
+      this.logger.log(`load "${title}" from drafts cache`);
+      callback(null, this.drafts[title]);
     } else if (!isSystemTiddler(title)) {
-      this.db.findOne({ title: title }, function (err, doc) {
-        self.logger.log("fetched " + JSON.stringify(doc));
+      this.db.findOne({title}, function (err, doc) {
         delete doc["_id"];
         if (err) {
           callback(err);
         } else {
+          self.logger.log(`load "${doc['title']}" from NeDB`);
           callback(null, documentToTiddlerFields(doc));
         }
       });
     } else {
+      this.logger.log(`load "${title}" when start`);
       callback(null, null);
     }
   };
 
   /**
    * Delete a tiddler and invoke the callback with (err)
-   * 
+   *
    * @param {*} title Title of tiddler to be deleted
    * @param {*} callback function invoked with parameter err
-   * @param {*} options 
+   * @param {*} options
    */
   FileSystemAdaptor.prototype.deleteTiddler = function(title,callback,options) {
     var self = this,
     fileInfo = this.boot.files[title];
-    this.logger.log(`delete "${title}"`);
 
     if(fileInfo) { // delete the system tiddler on disk
       $tw.utils.deleteTiddlerFile(fileInfo,function(err,fileInfo) {
@@ -259,71 +251,61 @@ A sync adaptor module for synchronising with the local filesystem via node.js AP
         }
         // Remove the tiddler from self.boot.files & return null adaptorInfo
         delete self.boot.files[title];
+        self.logger.log(`delete "${title}" from filesystem`);
         return callback(null,null);
       });
-    } else if (isDraftTiddler(title)) {
+    } else if (this.drafts[title]) {
       delete this.drafts[title];
+      self.logger.log(`delete "${title}" from drafts cache`);
       callback(null, null);
     } else if (this.memcache[title]) {
       delete this.memcache[title];
+      self.logger.log(`delete "${title}" from in-memory cache`);
       callback(null, null);
     } else {
       this.db.remove({ title: title }, { multi: true }, function (err, numRemoved) {
-        // if (numRemoved > 0) self.logger.log(""+numRemoved+" "+title+" is removed");
-        if (numRemoved > 0) self.logger.log(`${numRemoved} "${title}" ${numRemoved > 1 ? "are" : "is"} removed`);
+        if (numRemoved > 0) self.logger.log(`delete "${title}" from NeDB.`);
         callback(err, null);
       });
     }
   };
 
   /**
-   * Gets the revision ID associated with the specified tiddler title.
-   * 
-   * @param {string} title Tiddler title
-   * @returns revision ID.
-   */
-  //FileSystemAdaptor.prototype.getTiddlerRevision = function(title) {}
-
-  /**
-   * Attempts to login to the server with specified credentials. This method is optional.
-   * 
-   * @param {string} username 
-   * @param {string} password 
-   * @param {Function} callback Callback function invoked with parameter `err`
-   */
-  //FileSystemAdaptor.prototype.login = function(username,password,callback) {}
-
-  /**
-   * Invoked by the syncer to display a custom login promopt. This method is optional.
-   * The custom login prompt should send the widget message tm-login with the username and password in parameters username and password.
-   * 
-   * @param {object} syncer Reference to the syncer object making the call
-   */
-  //FileSystemAdaptor.prototype.displayLoginPrompt = function(syncer) {}
-
-  /**
-   * Attempts to logout of the server. This method is optional.
-   * 
-   * @param {Function} callback function invoked with parameter `err`
-   */
-  //FileSystemAdaptor.prototype.logout = function(callback) {}
-
-  /**
    * Retrieves the titles of tiddlers that need to be updated from the server.
-   * This method is optional. If an adaptor doesn't implement it then synchronisation will be unidirectional from the TiddlyWiki store to the adaptor,
+   * This method is optional.
+   * If an adaptor doesn't implement it then synchronisation will be unidirectional from the TiddlyWiki store to the adaptor,
    * but not the other way.
    * The syncer will use the getUpdatedTiddlers() method in preference to the getSkinnyTiddlers() method.
-   * 
+   *
    * The data provided by the callback is as follows:
-   * 
+   *
    * {
    *   modifications: [<array of title>],
    *   deletions: [<array of title>],
    * }
+   *
+   * tiddlerExists
+   *
    * @param {*} syncer Reference to the syncer object making the call
-   * @param {*} callback  function invoked with parameter err,data – see below
+   * @param {Function} callback function invoked with parameter err,data
    */
-  //FileSystemAdaptor.prototype.getUpdatedTiddlers = function(syncer,callback) {}
+  FileSystemAdaptor.prototype.getUpdatedTiddlers = function(syncer,callback) {
+    var self = this;
+    this.db.find({}, {title: 1}, function(err, docs) {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, {
+          modifications: docs.map(function (doc) {
+            return doc['title'];
+          }).filter(function (title) {
+            return !syncer.wiki.tiddlerExists(title);
+          }),
+          deletions: []
+        })
+      }
+    });
+  }
 
   /**
    * Retrieves a list of skinny tiddlers from the server.
@@ -331,10 +313,44 @@ A sync adaptor module for synchronising with the local filesystem via node.js AP
    * it then synchronisation will be unidirectional from the
    * TiddlyWiki store to the adaptor, but not the other way.
    * The syncer will use the getUpdatedTiddlers() method in preference to the getSkinnyTiddlers() method.
-   * 
+   *
    * @param {*} callback function invoked with parameter err,tiddlers, where tiddlers is an array of tiddler field objects
    */
   //FileSystemAdaptor.prototype.getSkinnyTiddlers = function(callback) {}
+
+  /**
+   * Gets the revision ID associated with the specified tiddler title.
+   *
+   * @param {string} title Tiddler title
+   * @returns revision ID.
+   */
+  //FileSystemAdaptor.prototype.getTiddlerRevision = function(title) {}
+
+  /**
+   * Attempts to login to the server with specified credentials.
+   * This method is optional.
+   *
+   * @param {string} username
+   * @param {string} password
+   * @param {Function} callback Callback function invoked with parameter `err`
+   */
+  //FileSystemAdaptor.prototype.login = function(username,password,callback) {}
+
+  /**
+   * Invoked by the syncer to display a custom login promopt.
+   * This method is optional.
+   * The custom login prompt should send the widget message tm-login with the username and password in parameters username and password.
+   *
+   * @param {object} syncer Reference to the syncer object making the call
+   */
+  //FileSystemAdaptor.prototype.displayLoginPrompt = function(syncer) {}
+
+  /**
+   * Attempts to logout of the server. This method is optional.
+   *
+   * @param {Function} callback function invoked with parameter `err`
+   */
+  //FileSystemAdaptor.prototype.logout = function(callback) {}
 
   if($tw.node) {
     exports.adaptorClass = FileSystemAdaptor;
